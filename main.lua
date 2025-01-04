ad493b4320c92b01820c8dd751519c85c9be8c48
angamer234k <tima.zhigachev@inbox.ru>
2025-01-04 19:48:11 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,9 +7,10 @@ local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local TextButton = Instance.new("TextButton")
 local UICorner = Instance.new("UICorner")
-local TextButton_2 = Instance.new("TextButton")
 local UICorner_2 = Instance.new("UICorner")
 local UIGradient = Instance.new("UIGradient")
+local TextButton_2 = Instance.new("TextButton")
+local ScrollingFrame = Instance.new("ScrollingFrame")
 local TextButton_3 = Instance.new("TextButton")
 
 --Properties:
@@ -21,8 +22,8 @@ Frame.Active = true
 Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.0604982264, 0, 0.0777777806, 0)
-Frame.Size = UDim2.new(0.147449598, 0, 0.366666675, 0)
+Frame.Position = UDim2.new(0.060498219, 0, 0.0777777806, 0)
+Frame.Size = UDim2.new(0.358600259, 0, 0.449999988, 0)
 
 TextButton.Parent = Frame
 TextButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
@@ -39,32 +40,40 @@ TextButton.TextWrapped = true
 
 UICorner.Parent = TextButton
 
-TextButton_2.Parent = Frame
-TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 87, 247)
-TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_2.BorderSizePixel = 0
-TextButton_2.Position = UDim2.new(0.0974821374, 0, 0.323232323, 0)
-TextButton_2.Size = UDim2.new(0.790748179, 0, 0.200000033, 0)
-TextButton_2.Font = Enum.Font.SourceSans
-TextButton_2.Text = "Infinite yield"
-TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_2.TextScaled = true
-TextButton_2.TextSize = 14.000
-TextButton_2.TextWrapped = true
-
 UICorner_2.Parent = Frame
 
 UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(107, 107, 107)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(77, 77, 77))}
 UIGradient.Parent = Frame
 
-TextButton_3.Parent = Frame
-TextButton_3.BackgroundColor3 = Color3.fromRGB(255, 38, 103)
+TextButton_2.Parent = Frame
+TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 38, 103)
+TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Position = UDim2.new(0.0974821523, 0, 0.263561547, 0)
+TextButton_2.Size = UDim2.new(0.790748179, 0, 0.200000033, 0)
+TextButton_2.Font = Enum.Font.SourceSans
+TextButton_2.Text = "PANIC BUTTON (REJOIN)"
+TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.TextScaled = true
+TextButton_2.TextSize = 14.000
+TextButton_2.TextWrapped = true
+
+ScrollingFrame.Parent = Frame
+ScrollingFrame.Active = true
+ScrollingFrame.BackgroundColor3 = Color3.fromRGB(83, 83, 83)
+ScrollingFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ScrollingFrame.BorderSizePixel = 0
+ScrollingFrame.Position = UDim2.new(0.0959312171, 0, 0.51028806, 0)
+ScrollingFrame.Size = UDim2.new(0, 239, 0, 100)
+
+TextButton_3.Parent = ScrollingFrame
+TextButton_3.BackgroundColor3 = Color3.fromRGB(255, 87, 247)
 TextButton_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
 TextButton_3.BorderSizePixel = 0
-TextButton_3.Position = UDim2.new(0.0974821374, 0, 0.601010084, 0)
-TextButton_3.Size = UDim2.new(0.790748179, 0, 0.200000033, 0)
+TextButton_3.Position = UDim2.new(0.104999997, 0, 0.00999999978, 0)
+TextButton_3.Size = UDim2.new(0.790748179, 0, 0.171193436, 0)
 TextButton_3.Font = Enum.Font.SourceSans
-TextButton_3.Text = "PANIC BUTTON (REJOIN)"
+TextButton_3.Text = "Infinite yield"
 TextButton_3.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextButton_3.TextScaled = true
 TextButton_3.TextSize = 14.000
@@ -72,30 +81,22 @@ TextButton_3.TextWrapped = true
 
 -- Scripts:
 
-local function XBUOV_fake_script() -- TextButton.panic 
+local function MXYI_fake_script() -- TextButton.panic 
 	local script = Instance.new('LocalScript', TextButton)
 
 	script.Parent.Activated:Connect(function() 
 		script.Parent.Parent.Parent:Destroy()
 	end)
 end
-coroutine.wrap(XBUOV_fake_script)()
-local function VMYSU_fake_script() -- Frame.drag 
+coroutine.wrap(MXYI_fake_script)()
+local function OLJCLF_fake_script() -- Frame.drag 
 	local script = Instance.new('LocalScript', Frame)
 
 	script.Parent.Draggable = true
 end
-coroutine.wrap(VMYSU_fake_script)()
-local function HSNVEDW_fake_script() -- TextButton_2.panic 
+coroutine.wrap(OLJCLF_fake_script)()
+local function MIVGQQ_fake_script() -- TextButton_2.panic 
 	local script = Instance.new('LocalScript', TextButton_2)
-
-	script.Parent.Activated:Connect(function() 
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-	end)
-end
-coroutine.wrap(HSNVEDW_fake_script)()
-local function JOGCH_fake_script() -- TextButton_3.panic 
-	local script = Instance.new('LocalScript', TextButton_3)
 
 	script.Parent.Activated:Connect(function() 
 		local TeleportService = game:GetService("TeleportService")
@@ -115,8 +116,16 @@ local function JOGCH_fake_script() -- TextButton_3.panic
 		coroutine.resume(Rejoin)
 	end)
 end
-coroutine.wrap(JOGCH_fake_script)()
-local function RLBMLCN_fake_script() -- ScreenGui.LocalScript 
+coroutine.wrap(MIVGQQ_fake_script)()
+local function SIIUI_fake_script() -- TextButton_3.panic 
+	local script = Instance.new('LocalScript', TextButton_3)
+
+	script.Parent.Activated:Connect(function() 
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+	end)
+end
+coroutine.wrap(SIIUI_fake_script)()
+local function MBADSST_fake_script() -- ScreenGui.LocalScript 
 	local script = Instance.new('LocalScript', ScreenGui)
 
 	function randomString()
@@ -132,4 +141,4 @@ local function RLBMLCN_fake_script() -- ScreenGui.LocalScript
 	
 	script.Parent.Name = randomString()
 end
-coroutine.wrap(RLBMLCN_fake_script)()
+coroutine.wrap(MBADSST_fake_script)()
